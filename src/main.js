aab8c1031952a8681581dcfee10f670b5ba63cdb
Michael Froelich <Michael.Froelich@unico.com.au>
2019-08-07 16:43:10 +1000

    1: beginning existing role checking

--- src/main.js
+++ src/main.js
@@ -9,35 +9,44 @@ const Log = require('./log.js');
 const fs = require('fs');
 const RoleParser = require('./roleparser.js');
 const Command = require('./command.js');
-const Token = fs.readFileSync(TokenFile, 'utf8');
 const client = new Discord.Client();
+var Token;
+try { 
+	Token = fs.readFileSync(TokenFile, 'utf8');
+} catch(err) {
+	console.log(err + " it's likely you need to add a file for your bot token.");
+}
 
-client.on('ready', () => {
-	client.user.setStatus('online');
-});
-
-client.on('message', message => {
-	if (message.content.startsWith(CommandCharacter)) {
-		Command.command(message);
-	}
-});
+client.on('ready', () => onStart());
+client.on('message', message => Command.command(message));
 client.on('messageReactionAdd', (reaction, user) => RoleParser.setRole(reaction, user));
 client.on('messageReactionRemove', (reaction, user) => RoleParser.removeRole(reaction, user));
 
 fs.watchFile(RolesFile, (curr, prev) => RoleParser.loadRoles(client));
 
-client.login(Token).then(value => {
+client.login(Token).then(value => onLogin(value), reason => {
+	Log.write('failed to login because\t' + reason.message);
+	process.exit(); //Exit the script
+});
+
+/**
+ * This function is run after the login function returns successfuly
+ */
+function onLogin(value){
 	fs.writeFile(PidFile, process.pid, function (err) {
 		if (err)
 			return console.log(err);
 	});
-
-	Log.write('started successfuly');
+	Log.write('Started successfuly');
 	RoleParser.loadRoles(client);
 	//Infinite loop, required for the file listener
 	setInterval(() => {
 	}, 1000)
-}, reason => {
-	Log.write('failed to login because\t' + reason.message);
-	process.exit(); //Exit the script
-});
+}
+
+/**
+ * This function is run after a "ready" signal from Discord
+ */
+function onStart(){
+	client.user.setStatus('online');
+}
--- src/roleparser.js
+++ src/roleparser.js
@@ -11,9 +11,14 @@ const DefaultRole = {
 };
 
 var Client;
+var Guilds;
 
+/**
+ * @param {Client} CurrentClient
+ */
 exports.loadRoles = function(CurrentClient) {
     Client = CurrentClient;
+    Guilds = Client.guilds;
     var roles = require(RolesFile);
     var defaultedRoles = Object.assign(DefaultRole, roles); //The root of the roles file forms the current default
 
@@ -25,11 +30,31 @@ exports.loadRoles = function(CurrentClient) {
 
 
 function setRoles(parentRole, currentRole){
-    Client;
+    checkIfRoleExists(currentRole);
+
+
     var roleName = currentRole[0];
     var defaultedRole = Object.assign(parentRole, currentRole[1]);
     for (let property in defaultedRole) {
+        for(let guild in Guilds.entries()) {
+            //guild.createRole();
+            console.log('start making roles here');
+        }
     }
+}
+
+/**
+ * TODO: some fuzzy detection here
+ */
+function checkIfRoleExists(currentRole) {
+    let entree = Guilds.entries();
+    for(let guild of Guilds.entries()) {
+        for(let role of guild[1].roles.entries()) {
+            //guild.createRole();
+            console.log('start making roles here');
+        }
+    }
+    //czechking
 }
 
 exports.setRole = function() {
