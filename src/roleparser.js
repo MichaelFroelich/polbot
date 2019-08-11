0056349e211ed96cc9bfbff8a18b8844a5296f6d
Michael Froelich <MichaelFroelich@users.noreply.github.com>
2019-08-11 14:05:50 +1000

    1: updates roles lists and role logic

--- src/roleparser.js
+++ src/roleparser.js
@@ -16,47 +16,70 @@ var Guilds;
 /**
  * @param {Client} CurrentClient
  */
-exports.loadRoles = function(CurrentClient) {
+exports.loadRoles = function (CurrentClient) {
     Client = CurrentClient;
     Guilds = Client.guilds;
     var roles = require(RolesFile);
     var defaultedRoles = Object.assign(DefaultRole, roles); //The root of the roles file forms the current default
 
-    for(let role of Object.entries(roles.roles)) {
-        setRoles(roles, role);
+    for (let role of Object.entries(defaultedRoles.roles)) {
+        setRoles(defaultedRoles, role);
     }
 }
 
-function setRoles(parentRole, currentRole){
-    checkIfRoleExists(currentRole);
+function setRoles(parentRole, currentRole) {
     var roleName = currentRole[0];
     var defaultedRole = Object.assign(parentRole, currentRole[1]);
-    for (let property in defaultedRole) {
-        for(let guild in Guilds.values()) {
-            //guild.createRole();
-            console.log('start making roles here');
+    defaultedRole.roles = currentRole[1].roles;
+    if (defaultedRole.roles !== undefined) {
+        for (let role of Object.entries(defaultedRole.roles)) {
+            setRoles(defaultedRole, role);
         }
     }
+    createRole(defaultedRole); 
+}
+
+function createRole(defaultedRole, ) {
+    var mappedRole = mapRole(defaultedRole);
+    /*
+    for (let property in defaultedRole) {
+        for (let guild in Guilds.values()) {
+            let existentRole = checkIfRoleExists(currentRole, guild);
+            if (existentRole != null) {
+                existentRole.edit();
+            }
+            else {
+                guild.createRole();
+            }
+            console.log('start making roles here');
+        }
+    }*/
+}
+
+function mapRole(role) {
+    return {
+
+    };
 }
 
 /**
  * TODO: some fuzzy detection here
  */
 function checkIfRoleExists(currentRole) {
-    for(let guild of Guilds.values()) {
-        for(let role of guild.roles.values()) {
-            guild.createRole();
+    for (let guild of Guilds.values()) {
+        for (let role of guild.roles.values()) {
+            //guild.createRole();
             console.log('start making roles here');
         }
     }
     //czechking
 }
 
-exports.setRole = function() {
+exports.setRole = function () {
     return "Hello";
 }
 
-exports.removeRole = function() {
+exports.removeRole = function () {
     return "Hello";
 }
 
